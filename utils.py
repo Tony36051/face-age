4b2107bf65feff0e1743aab0a3b8e4dff6a82008
tony36051 <360517703@163.com>
2018-04-18 21:47:55 +0800

    tune

--- utils.py
+++ utils.py
@@ -23,8 +23,8 @@ def get_meta(mat_path, db):
     photo_taken = meta[db][0, 0]["photo_taken"][0]  # year
     face_score = meta[db][0, 0]["face_score"][0]
     second_face_score = meta[db][0, 0]["second_face_score"][0]
-    age = np.array(map(calc_age, photo_taken, dob))
-    # age = [calc_age(photo_taken[i], dob[i]) for i in range(len(dob))]
+    # age = np.array(map(calc_age, photo_taken, dob))  # python 2/3 staff
+    age = [calc_age(photo_taken[i], dob[i]) for i in range(len(dob))]
     return full_path, dob, gender, photo_taken, face_score, second_face_score, age
 
 
@@ -33,6 +33,17 @@ def mk_dir(dir):
         os.mkdir(dir)
     except OSError:
         pass
+
+
+def read_meta_data(data_dir, file_name):
+    path = list()
+    age = list()
+    with open(os.path.join(data_dir, file_name)) as f:
+        for line in f.readlines():
+            ss = line.split(" ")
+            path.append(ss[0])
+            age.append(int(ss[1]))
+    return path, age
 
 
 if __name__ == '__main__':
